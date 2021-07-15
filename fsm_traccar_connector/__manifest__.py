f6e9c9814834c2fe9bd26dae8cf4360fd7aa9273
nicolasrsande <nicolasrsande@gmail.com>
2021-07-15 13:51:08 -0300

    Initial tree

---
+++ fsm_traccar_connector/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2021 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Field Service Traccar Connector',
+    'version': '13.0.1.0.0',
+    'description': 'Connects Traccar Server Api to odoo field service module.',
+    'summary': 'This module allows you to connect Traccar api to field service module and adds new features for interaction between the two.',
+    'author': 'Nimarosa',
+    'website': 'www.github.com/nimarosa',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'depends': [
+        ''
+    ],
+    'data': [
+        ''
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
