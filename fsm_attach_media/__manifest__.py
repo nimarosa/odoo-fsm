9711e1e43f0d00204f1fc73870030a462f9fabd0
nicolasrsande <nicolasrsande@gmail.com>
2021-08-19 21:08:46 -0300

    merge new names

--- fsm_attach_media/__manifest__.py
+++ fsm_attach_media/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'FSM Attach Media',
+    'name': 'Fieldservice - Attach Media',
     'version': '13.0.1.0.0',
     'description': 'FSM module to support attaching images to the order',
     'summary': 'FSM module to support attaching images to the order',
--- fsm_signature/__manifest__.py
+++ fsm_signature/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'FSM Signature',
+    'name': 'Fieldservice - Signature',
     'version': '13.0.1.0.0',
     'description': 'FSM Module to support signature',
     'summary': 'FSM Sign your FSM orders',
